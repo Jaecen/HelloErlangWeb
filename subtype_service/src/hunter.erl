e42c7dacec91a6d21a15570f9549f23f69a52ea2
Jason Addington <jason@addington.com>
2012-05-07 23:37:13 -0700

    First working version.

--- subtype_service/src/hunter.erl
+++ subtype_service/src/hunter.erl
@@ -1,7 +1,35 @@
 -module(hunter).
+-export([get_cards_of_type_for_set/2]).
 
--export([get_cards_for_set/1]).
+build_auth_header(Username, Password) ->
+	Encoded = base64:encode_to_string(lists:append([Username,":",Password])),
+	{"Authorization","Basic " ++ Encoded}.
 
-get_cards_for_set(set_name) ->
+build_content_type_header(Content_Type) ->
+	{"Content-Type", Content_Type}.
+
+get_cards_for_set(Set) ->
+	Uri = string:concat("http://hunter.cardnex.us/sets/", http_uri:encode(Set)),
+	Headers = [
+		build_auth_header("auth", "0r1z3d"), 
+		build_content_type_header("application/json")],
 	inets:start(),
-	{ok, {{Version, 200, ReasonPhrase}, Headers, Body}} = httpc:request("http://www.google.com").
+	{ok, {200, Body}} = httpc:request(get, {Uri, Headers}, [], [{full_result, false}]),
+	
+	{struct,[_|[{"cards", {array, Cards}}|_]]} = mochijson:decode(Body),
+	Cards.
+
+get_cards_of_type_for_set(Type, Set) ->
+	Cards = get_cards_for_set(Set),
+	CardsOfType = lists:filter(
+		fun({struct, Details}) -> 
+			lists:any(
+				fun({"spoilerType",Value}) ->
+					string:str(string:to_lower(Value), string:to_lower(Type)) =/= 0;
+				({_,_}) ->
+					false
+				end,
+				Details) 
+		end, 
+		Cards),
+	lists:map(fun({struct, Card}) -> Card end, CardsOfType).
--- subtype_service/src/subtype_service_resource.erl
+++ subtype_service/src/subtype_service_resource.erl
@@ -12,5 +12,21 @@ init([]) -> {ok, undefined}.
 content_types_provided(ReqData, Context) ->
 	{[{"application/json",to_json}, {"text/html",to_json}], ReqData, Context}.
 
-to_json(ReqData, Context) ->
-	{"blorb", ReqData, Context}.
+to_json(ReqData, Context) ->	
+	InnCards = hunter:get_cards_of_type_for_set(wrq:path_info(subtype, ReqData), "Innistrad"),
+	DkaCards = hunter:get_cards_of_type_for_set(wrq:path_info(subtype, ReqData), "Dark Ascension"),
+	AvrCards = hunter:get_cards_of_type_for_set(wrq:path_info(subtype, ReqData), "Avacyn Restored"),
+	
+	CardMapper = fun(Card) ->
+			{_, Name} = lists:keyfind("checklistName", 1, Card),
+			{_, Color} = lists:keyfind("checklistColor", 1, Card),
+			{struct, [{"name", Name}, {"color", Color}]}
+			end,
+	
+	MappedCards = {array, 
+		lists:map(CardMapper, InnCards) ++
+		lists:map(CardMapper, DkaCards) ++
+		lists:map(CardMapper, AvrCards)	
+	},
+
+	{mochijson:encode(MappedCards), ReqData, Context}.
